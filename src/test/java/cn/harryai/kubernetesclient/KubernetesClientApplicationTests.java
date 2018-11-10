2beecf51e0c818fff9b85190a6ebcae0a3451f3e
harry <hhr_harry@qq.com>
2018-11-10 09:46:21 +0800

    first

---
+++ src/test/java/cn/harryai/kubernetesclient/KubernetesClientApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.harryai.kubernetesclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KubernetesClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
