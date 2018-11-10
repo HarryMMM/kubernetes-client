795535e489cdfac0cd0937cee4585ac163505c1b
harry <hhr_harry@qq.com>
2018-11-10 10:20:38 +0800

    t

--- src/main/java/cn/harryai/kubernetesclient/Testt.java
+++ src/main/java/cn/harryai/kubernetesclient/Testt.java
@@ -1,4 +1,24 @@
 package cn.harryai.kubernetesclient;
 
-public class Testt {
+import io.fabric8.kubernetes.api.model.*;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+
+public class Testt
+{
+    public static void main(String[] args) {
+        Config config = new ConfigBuilder().withMasterUrl("https://mymaster.com").build();
+        KubernetesClient client = new DefaultKubernetesClient(config);
+        final NamespaceList list = client.namespaces().list();
+        Service service = client.services().inNamespace("").withName("").get();
+        Pod pod = client.pods().inNamespace("").withName("").get();
+        Namespace namespace = client.namespaces().withName("").get();
+        client.resourceQuotas().inNamespace("");
+
+
+    }
 }
